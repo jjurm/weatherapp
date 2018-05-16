6d101fed99e81bebb2bd8464b8c32239c87e62d7
enchom <39163881+enchom@users.noreply.github.com>
2018-05-16 21:04:49 +0100

    Implemented Data Downloader
    Some changes in WeatherData and WeatherDataDownloader interfaces.

--- src/main/java/uk/ac/cam/intdesign/group10/weatherapp/location/Location.java
+++ src/main/java/uk/ac/cam/intdesign/group10/weatherapp/location/Location.java
@@ -5,6 +5,11 @@ public class Location {
     // example: "Cambridge, UK"
 
     String location;
+    
+    public String getLocation()
+    {
+    	return location;
+    }
 
     public Location(String location) {
         this.location = location;
--- src/main/java/uk/ac/cam/intdesign/group10/weatherapp/weather/WeatherDataDownloader.java
+++ src/main/java/uk/ac/cam/intdesign/group10/weatherapp/weather/WeatherDataDownloader.java
@@ -2,9 +2,11 @@ package uk.ac.cam.intdesign.group10.weatherapp.weather;
 
 import java.io.IOException;
 
+import uk.ac.cam.intdesign.group10.weatherapp.location.Location;
+
 public interface WeatherDataDownloader {
 
-    public void fetchData();
+    public void fetchData(Location loc);
 
     public void subscribe(Observer observer);
     public void unsubscribe(Observer observer);
