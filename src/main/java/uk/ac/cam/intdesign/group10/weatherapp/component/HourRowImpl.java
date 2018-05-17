8a0f4ddabcdbb586e567a37f64bd29009768974a
Mara <mm2260@cam.ac.uk>
2018-05-17 14:19:34 +0100

    acceptWeatherData doesn't work with this

--- src/main/java/uk/ac/cam/intdesign/group10/weatherapp/component/HourRowImpl.java
+++ src/main/java/uk/ac/cam/intdesign/group10/weatherapp/component/HourRowImpl.java
@@ -12,62 +12,53 @@ import uk.ac.cam.intdesign.group10.weatherapp.weather.WeatherData.DayInfo;
 import uk.ac.cam.intdesign.group10.weatherapp.weather.WeatherData.HourInfo;
 import uk.ac.cam.intdesign.group10.weatherapp.weather.WeatherDataConsumer;
 
-public class HourRowImpl extends JPanel implements HourRow, WeatherDataConsumer{
-	
-	
+public class HourRowImpl extends JPanel implements HourRow, WeatherDataConsumer {
+
 	public WeatherData weatherData;
 	public int fromHour;
 	public int toHour;
 	public int day = Calendar.DAY_OF_MONTH;
 	public HourInfo hour;
-	
+
 	public HourRowImpl(int startHourOfROw, int endHourOfRow) {
 		fromHour = startHourOfROw;
 		toHour = endHourOfRow;
-		
-		
+
 	}
 
-	
 	public JLabel hoursLabel() {
 		JLabel jHour = new JLabel();
-		if(fromHour < 10) {
-			if(toHour < 10) {
-			jHour.setText("0" + String.valueOf(fromHour) + " - " + "0"+ String.valueOf(toHour));
+		if (fromHour < 10) {
+			if (toHour < 10) {
+				jHour.setText("0" + String.valueOf(fromHour) + " - " + "0" + String.valueOf(toHour));
 			} else {
-				
+
 				jHour.setText("0" + String.valueOf(fromHour) + " - " + String.valueOf(toHour));
-				
+
 			}
 		} else {
 			jHour.setText(String.valueOf(fromHour) + " - " + String.valueOf(toHour));
 		}
 		return jHour;
-		
+
 	}
-	
 
 	public JLabel temperatureLabel() {
 		JLabel jTemperature = new JLabel();
-		
+
 		return jTemperature;
 	}
-	
-	
-	
+
 	@Override
 	public JComponent getRootComponent() {
-		return this ;
+		return this;
 	}
 
 	@Override
 	public void acceptWeatherData(WeatherData data) {
-		
-		weatherData = data ;
-		
-		
+
+		weatherData = data;
+
 	}
-	
-	
 
 }
--- src/main/java/uk/ac/cam/intdesign/group10/weatherapp/content/DayContentPanel.java
+++ src/main/java/uk/ac/cam/intdesign/group10/weatherapp/content/DayContentPanel.java
@@ -16,8 +16,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
-
-
 import uk.ac.cam.intdesign.group10.weatherapp.component.test.TestComponent;
 import uk.ac.cam.intdesign.group10.weatherapp.weather.WeatherData;
 import uk.ac.cam.intdesign.group10.weatherapp.weather.WeatherData.DayInfo;
@@ -27,167 +25,162 @@ import uk.ac.cam.intdesign.group10.weatherapp.component.*;
 import uk.ac.cam.intdesign.group10.weatherapp.weather.*;
 
 public class DayContentPanel extends JPanel implements ContentPanel {
-	public WeatherData weatherData;
-	public WeatherData.DayInfo day;
-    public List<HourRowImpl> rows;
-    public Map<HourRowImpl, Double> estimations;
-    public SuitabilityEstimatorImpl estimator;
-    public DayContentPanel() {
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        // just for test, remove it
-        add(new TestComponent("Day overview", Color.pink));
-        JPanel suggestedTime = suggestedTimeCreator();
-        this.add(suggestedTime);
-        rows = initialiseHourRows();
-        
-        for(int i = 0 ; i < 24 ; i ++) {
-        	
-        	this.add(rows.get(i));
-        }
-        for(HourRowImpl hr : rows) {
-        	if(hr.getBackground().equals(Color.GREEN)) {
-        		int h1 = hr.fromHour;
-        		int h2 = hr.toHour;
-        		JLabel suggestedHours = new JLabel();
-        		if(h1 < 10) {
-        			if(h2 < 10) {
-        			suggestedHours.setText("0" + String.valueOf(h1) + " - " + "0"+ String.valueOf(h2));
-        			} else {
-        				
-        				suggestedHours.setText("0" + String.valueOf(h1) + " - " + String.valueOf(h2));
-        				
-        			}
-        		} else {
-        			suggestedHours.setText(String.valueOf(h1) + " - " + String.valueOf(h2));
-        		}
-        		suggestedTime.add(suggestedHours);
-        		break;
-        		
-        	}
-        }
-        
-    }
 
-    @Override
-    public JComponent getRootComponent() {
-        return this;
-    }
+	public List<HourRowImpl> rows;
+	public Map<HourRowImpl, Double> estimations;
 
-    @Override
-    public void acceptWeatherData(WeatherData data) {
-        
-    	weatherData = data;
-    }
-    
-	
-	public HourInfo getHourInfo(HourRowImpl hr) {
+	public DayContentPanel() {
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		// just for test, remove it
+		add(new TestComponent("Day overview", Color.pink));
+
+	}
+
+	@Override
+	public JComponent getRootComponent() {
+		return this;
+	}
+
+	@Override
+	public void acceptWeatherData(WeatherData data) {
+
+		putContentInPanel(data);
+		System.out.println("here");
+	}
+
+	public void putContentInPanel(WeatherData data) {
+		JPanel suggestedTime = suggestedTimeCreator();
+		
+		rows = initialiseHourRows(data);
+
+		for (int i = 0; i < 24; i++) {
+
+			this.add(rows.get(i));
+		}
+		for (HourRowImpl hr : rows) {
+			if (hr.getBackground().equals(Color.GREEN)) {
+				int h1 = hr.fromHour;
+				int h2 = hr.toHour;
+				JLabel suggestedHours = new JLabel();
+				if (h1 < 10) {
+					if (h2 < 10) {
+						suggestedHours.setText("0" + String.valueOf(h1) + " - " + "0" + String.valueOf(h2));
+					} else {
+
+						suggestedHours.setText("0" + String.valueOf(h1) + " - " + String.valueOf(h2));
+
+					}
+				} else {
+					suggestedHours.setText(String.valueOf(h1) + " - " + String.valueOf(h2));
+				}
+				suggestedTime.add(suggestedHours);
+				continue;
+
+			}
+		}
+		this.add(suggestedTime);
+	}
+
+	public HourInfo getHourInfo(HourRowImpl hr, WeatherData weatherData) {
 		HourInfo hourInfo = new HourInfo();
 		List<DayInfo> days = weatherData.days;
-		for(DayInfo d : days) {
-			if(d.day.getDayOfMonth() == hr.day){
-				
+		for (DayInfo d : days) {
+			if (d.day.getDayOfMonth() == hr.day) {
+
 				List<HourInfo> hours = d.hours;
-				for(HourInfo h : hours) {
-					if(hr.fromHour == h.time.getHour()) {
-						
+				for (HourInfo h : hours) {
+					if (hr.fromHour == h.time.getHour()) {
+
 						hourInfo = h;
-						
+
 					}
 				}
 			}
-			
+
 			break;
 		}
 		return hourInfo;
-		
-		
-		
+
 	}
-    
-    
-    
-    public List<HourRowImpl> initialiseHourRows() {
-    	
-    	List<HourRowImpl> hourRows = new ArrayList<HourRowImpl>();
-    	Map<HourRowImpl,Double> estimations = new HashMap<HourRowImpl,Double>();
-    	
-    	for( int i = 0 ; i < 24 ; i ++) {
-    		HourRowImpl hr = new HourRowImpl(i,i+1);
-    		hr.setLayout(new FlowLayout(FlowLayout.LEFT, 40, 0));
-    		JLabel jHours = hr.hoursLabel();
-    		JLabel jTemperature = hr.temperatureLabel();
-    		HourInfo hourInfo = getHourInfo(hr);
-    		double hourTemperature = hourInfo.temperature;
-    		double estimation = Math.abs(20 - hourTemperature);
-    		estimations.put(hr,estimation);
-    		jTemperature.setText(String.valueOf(hourTemperature) + " \u00b0" + "C");
-    		hr.add(jHours);
-    		hr.add(jTemperature);
-    		hourRows.add(hr);
-    		
-    	}
-    	normalise();
-    	colourHourRows();
-    	return hourRows;
-    }
-    
-    public void normalise() {
-    	
-    
-    	double maximum = java.util.Collections.max(estimations.values());
-    	double minimum = java.util.Collections.min(estimations.values());
-    	for(HourRowImpl hr : estimations.keySet()) {
-    		double val = estimations.get(hr);
-    		double normVal = (val - minimum)/(maximum - minimum);
-    		estimations.put(hr, normVal);
-    	}
-    }
-    
-    public void colourHourRows() {
-    	
-    	for(HourRowImpl hr : rows) {
-    		if(getHourInfo(hr).type.equals(WeatherType.RAINY)) {
-        		hr.setBackground(setColor(0.0));
-        		} else {
-                hr.setBackground(setColor(estimations.get(hr)));
-        		}
-    	}
-    	
-    }
-   
-    public Color setColor(double est) {
-    	//Hue
-    	double h = est*0.4;
-    	//Saturation
-    	double s = 0.9;
-    	//Brightness
-    	double b = 0.9;
-    	return Color.getHSBColor((float)h, (float)s, (float)b) ;
-    }
-    
- 
-    
-    public JPanel suggestedTimeCreator() {
-    	JPanel jp = new JPanel();
-    	jp.setLayout(new FlowLayout(FlowLayout.LEFT,40,0));
-    	JLabel st = new JLabel("Suggested time:");
-    	jp.add(st);
-    	double from = 0.0;
-    	double to = 0.0;
-    	for(HourRowImpl hr : estimations.keySet()) {
-    		if(estimations.get(hr) == 1.0) {
-    			 from = hr.fromHour;
-    			 to = hr.toHour;
-    			 break;
-    		}
-    		
-    		
-    	}
-    	JLabel jTime = new JLabel();
-    	jTime.setText(String.valueOf(from) + " - " + String.valueOf(to));
-    	
-    	return jp;
-    }
-    
-    
+
+	public List<HourRowImpl> initialiseHourRows(WeatherData data) {
+
+		List<HourRowImpl> hourRows = new ArrayList<HourRowImpl>();
+		Map<HourRowImpl, Double> estimations = new HashMap<HourRowImpl, Double>();
+
+		for (int i = 0; i < 24; i++) {
+			HourRowImpl hr = new HourRowImpl(i, i + 1);
+			hr.setLayout(new FlowLayout(FlowLayout.LEFT, 40, 0));
+			JLabel jHours = hr.hoursLabel();
+			JLabel jTemperature = hr.temperatureLabel();
+			HourInfo hourInfo = getHourInfo(hr, data);
+			double hourTemperature = hourInfo.temperature;
+			double estimation = Math.abs(20 - hourTemperature);
+			estimations.put(hr, estimation);
+			jTemperature.setText(String.valueOf(hourTemperature) + " \u00b0" + "C");
+			hr.add(jHours);
+			hr.add(jTemperature);
+			hourRows.add(hr);
+
+		}
+		normalise();
+		colourHourRows(data);
+		return hourRows;
+	}
+
+	public void normalise() {
+
+		double maximum = java.util.Collections.max(estimations.values());
+		double minimum = java.util.Collections.min(estimations.values());
+		for (HourRowImpl hr : estimations.keySet()) {
+			double val = estimations.get(hr);
+			double normVal = (val - minimum) / (maximum - minimum);
+			estimations.put(hr, normVal);
+		}
+	}
+
+	public void colourHourRows(WeatherData data) {
+
+		for (HourRowImpl hr : rows) {
+			if (getHourInfo(hr,data).type.equals(WeatherType.RAINY)) {
+				hr.setBackground(setColor(0.0));
+			} else {
+				hr.setBackground(setColor(estimations.get(hr)));
+			}
+		}
+
+	}
+
+	public Color setColor(double est) {
+		// Hue
+		double h = est * 0.4;
+		// Saturation
+		double s = 0.9;
+		// Brightness
+		double b = 0.9;
+		return Color.getHSBColor((float) h, (float) s, (float) b);
+	}
+
+	public JPanel suggestedTimeCreator() {
+		JPanel jp = new JPanel();
+		jp.setLayout(new FlowLayout(FlowLayout.LEFT, 40, 0));
+		JLabel st = new JLabel("Suggested time:");
+		System.out.println("here");
+		jp.add(st);
+		double from = 0.0;
+		double to = 0.0;
+		for (HourRowImpl hr : estimations.keySet()) {
+			if (estimations.get(hr) == 1.0) {
+				from = hr.fromHour;
+				to = hr.toHour;
+				continue;
+			}
+
+		}
+		JLabel jTime = new JLabel();
+		jTime.setText(String.valueOf(from) + " - " + String.valueOf(to));
+
+		return jp;
+	}
+
 }
