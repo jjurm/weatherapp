24d810104dffc500ee2acb4299c9e0f5d6c5c78d
enchom <39163881+enchom@users.noreply.github.com>
2018-05-18 15:52:36 +0100

    tab bar implemented

--- src/main/java/uk/ac/cam/intdesign/group10/weatherapp/component/TabBarImpl.java
+++ src/main/java/uk/ac/cam/intdesign/group10/weatherapp/component/TabBarImpl.java
@@ -2,19 +2,70 @@ package uk.ac.cam.intdesign.group10.weatherapp.component;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.GridLayout;
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 
+import uk.ac.cam.intdesign.group10.weatherapp.WeatherApp;
 import uk.ac.cam.intdesign.group10.weatherapp.component.test.TestComponent;
+import uk.ac.cam.intdesign.group10.weatherapp.content.DayContentPanel;
+import uk.ac.cam.intdesign.group10.weatherapp.content.OverviewContentPanel;
+import uk.ac.cam.intdesign.group10.weatherapp.screen.HomeScreen;
 import uk.ac.cam.intdesign.group10.weatherapp.weather.WeatherData;
 
-public class TabBarImpl extends JPanel implements TabBar {
-
-    public TabBarImpl() {
-        setLayout(new BorderLayout());
-        // just for test, remove it for real implementation
-        add(new TestComponent("TabBar", Color.LIGHT_GRAY));
+public class TabBarImpl extends JPanel implements TabBar
+{
+	private int activeBar = 0;
+	private List<TabBarComponent> tabs;
+	private HomeScreen screen;
+	private WeatherApp app;
+	
+    public TabBarImpl(WeatherApp wap, HomeScreen scr)
+    {
+        setLayout(new GridLayout());
+        
+        screen = scr;
+        app = wap;
+        
+    	LocalDate date = LocalDate.now().plusDays(2);
+    	String DoW = date.getDayOfWeek().toString().substring(0, 1) + date.getDayOfWeek().toString().substring(1, date.getDayOfWeek().toString().length()).toLowerCase();
+    	
+    	tabs = new LinkedList<>();
+    	
+    	tabs.add(new TabBarComponent("Overview", Color.LIGHT_GRAY, Color.WHITE, this, 0));
+    	tabs.add(new TabBarComponent("Today", Color.LIGHT_GRAY, Color.WHITE, this, 1));
+    	tabs.add(new TabBarComponent("Tomorrow", Color.LIGHT_GRAY, Color.WHITE, this, 2));
+    	tabs.add(new TabBarComponent(DoW, Color.LIGHT_GRAY, Color.WHITE, this, 3));
+    	date = date.plusDays(1);
+        DoW = date.getDayOfWeek().toString().substring(0, 1) + date.getDayOfWeek().toString().substring(1, date.getDayOfWeek().toString().length()).toLowerCase();
+    	tabs.add(new TabBarComponent(DoW, Color.LIGHT_GRAY, Color.WHITE, this, 4));
+    	
+    	for (TabBarComponent tab : tabs)
+    	{
+    		add(tab);
+    	}
+    	
+    	tabs.get(0).activate();
+    }
+    
+    public void tabBarChange(int id)
+    {
+    	if (id == activeBar)
+    		return;
+    	
+    	if (id == 0)
+    		screen.changeContentPanel( new OverviewContentPanel(app, screen) );
+    	else
+    		screen.changeContentPanel( new DayContentPanel(id-1) );
+    	
+    	tabs.get(activeBar).deactivate();
+    	tabs.get(id).activate();
+    	
+    	activeBar = id;
     }
 
     @Override
