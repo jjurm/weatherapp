328dbc7749886da5ab2b4e60c28a13daac965f2a
ymohamedahmed <ygaafar@mail.com>
2018-05-21 22:57:00 +0100

    modified day summary to use javafx

--- src/main/java/uk/ac/cam/intdesign/group10/weatherapp/component/DaySummary.java
+++ src/main/java/uk/ac/cam/intdesign/group10/weatherapp/component/DaySummary.java
@@ -1,6 +1,7 @@
 package uk.ac.cam.intdesign.group10.weatherapp.component;
 
+import uk.ac.cam.intdesign.group10.weatherapp.component.test.FxComponent;
 import uk.ac.cam.intdesign.group10.weatherapp.weather.WeatherDataConsumer;
 
-public interface DaySummary extends AppComponent, WeatherDataConsumer {
+public interface DaySummary extends FxComponent, WeatherDataConsumer {
 }
